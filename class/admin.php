5546b57f69b1ad497a042ade2f3e64f38b1a0cf2
Minh Ý Nguyễn <minhynguyen97@gmail.com>
2022-10-31 21:48:15 +0700

    make it better

--- class/admin.php
+++ class/admin.php
@@ -32,27 +32,28 @@
 			//Tên db: shoeDatabase
 			//usernname: admin
 			//password: 123qwe!@#
-			function connect(){
+
+			/*function connect(){
 			$con = new MySQLi('localhost','admin','123qwe!@#','shoeDatabase');	
 				if($con->connect_error){
 				die('Connection failed: '. $con->connect_error);
 				}
 			 return $con;
-			 }	
+			 }*/
 
-			/*private function connect(){
+			function connect(){
 				//Get Heroku ClearDB connection information
-				//$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-				//$cleardb_server = $cleardb_url["host"];
-				//$cleardb_username = $cleardb_url["user"];
-				//$cleardb_password = $cleardb_url["pass"];
-				//$cleardb_db = substr($cleardb_url["path"],1);
-				//$active_group = 'default';
-				//$query_builder = TRUE;
+				$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+				$cleardb_server = $cleardb_url["host"];
+				$cleardb_username = $cleardb_url["user"];
+				$cleardb_password = $cleardb_url["pass"];
+				$cleardb_db = substr($cleardb_url["path"],1);
+				$active_group = 'default';
+				$query_builder = TRUE;
 				// Connect to DB
-				//$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-				//return $con;
-			}*/
+				$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+				return $con;
+			}
 		
 			//Hàm thêm xóa sửa database
 			function product_modify($sql){
